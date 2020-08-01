f06402b76fc804088d4eadd7c77b8b65207e146b
ZXY <11510925@mail.sustc.edu.cn>
2020-07-31 18:33:42 -0700

    main.m comment out function LogImage

--- main.m
+++ main.m
@@ -109,14 +109,14 @@ toc;
 % end
 
 % LoG detector for particles detection 
-function img_log = LogImage(image)
-    
-    img = double(squeeze(image));
-    Log_filter = -fspecial('log', [10,10], 4.0); % fspecial creat predefined filter.Return a filter.
-                                       
-    img_log = imfilter(img, Log_filter, 'symmetric', 'conv');
- 
-end
+% function img_log = LogImage(image)
+%     
+%     img = double(squeeze(image));
+%     Log_filter = -fspecial('log', [10,10], 4.0); % fspecial creat predefined filter.Return a filter.
+%                                        
+%     img_log = imfilter(img, Log_filter, 'symmetric', 'conv');
+%  
+% end
 
 function imgxy = IdentifySpots(img, thresh)
 % Extract locations from image
