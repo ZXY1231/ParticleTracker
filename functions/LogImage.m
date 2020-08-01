6da286e2469dc724dbc0870b27ca1493d2add411
ZXY <11510925@mail.sustc.edu.cn>
2020-07-31 18:30:45 -0700

    LoG operation on images

---
+++ functions/LogImage.m
@@ -0,0 +1,8 @@
+function img_log = LogImage(image)
+    
+    img = double(squeeze(image));
+    Log_filter = -fspecial('log', [10,10], 4.0); % fspecial creat predefined filter.Return a filter.
+                                       
+    img_log = imfilter(img, Log_filter, 'symmetric', 'conv');
+ 
+end
