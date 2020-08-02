765102263c349b4566242a7a8a9ddfe48598c8f3
ZXY_win <zhouxy1231@gmail.com>
2020-08-02 00:00:02 -0700

    function can load the images stack as well

--- code/LoadImages.m
+++ code/LoadImages.m
@@ -1,4 +1,5 @@
 function all_images = LoadImages(source_path)
+try
     source_path
     imges = dir([source_path '*.tif']);
     imge_num = length(imges);
@@ -10,4 +11,16 @@ function all_images = LoadImages(source_path)
         all_images(i,:,:) = img;
 
     end
+catch
+    source_path
+    info = imfinfo(source_path);
+    num_images = numel(info);
+    all_images = zeros(num_images, info(1).Width, info(1).Height);
+    
+    for k = 1:num_images
+        img = imread(source_path, k);
+        all_images(k,:,:) = img;
+        % ... Do something with image A ...
+    end
+end
 end
