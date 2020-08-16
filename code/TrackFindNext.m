b6d00bde51dc7974770665ec96a881db1c4e4f94
ZXY_win <zhouxy1231@gmail.com>
2020-08-16 01:16:21 -0700

    provide multi-particles assignment temporary solution

--- code/TrackFindNext.m
+++ code/TrackFindNext.m
@@ -8,10 +8,11 @@ function find_spot = TrackFindNext(one_tracker, next_bright_particles, next_dim_
 %                                   providing dim spots detected at i-th frame with shape m2*2
 %         t                       : t indicateds t-th frame of detected spots
 %Output:  find_spot                    :[1, next spot ID] or 0
+%provide multi-particles assignment temporary solution
     
     global all_images_bright_particles all_images_dim_particles
     find_spot = one_tracker.FindNext(next_bright_particles);
-    if find_spot(1)
+    if find_spot(1) && all_images_bright_particles{t}(find_spot(2),3) == 0% multi-particles assignment temporary solution
         all_images_bright_particles{t}(find_spot(2),3) = 1;
     else 
         find_spot = one_tracker.FindNextDim(next_dim_particles);
