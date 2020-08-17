a4e6e69ba0391f454d440e3fe5fd46690ca0ad97
ZXY_win <zhouxy1231@gmail.com>
2020-08-16 22:54:08 -0700

    bug fixed, added positions will be previously unused

--- code/TrackFindNext.m
+++ code/TrackFindNext.m
@@ -8,13 +8,20 @@ function find_spot = TrackFindNext(one_tracker, next_bright_particles, next_dim_
 %                                   providing dim spots detected at i-th frame with shape m2*2
 %         t                       : t indicateds t-th frame of detected spots
 %Output:  find_spot                    :[1, next spot ID] or 0
-%provide multi-particles assignment temporary solution
+
+%provide multi-particles assignment temporary solution, bug: tracker adds
+%posirions before if find_spot()
     
     global all_images_bright_particles all_images_dim_particles
     find_spot = one_tracker.FindNext(next_bright_particles);
+    
     if find_spot(1) && all_images_bright_particles{t}(find_spot(2),3) == 0% multi-particles assignment temporary solution
+%     if find_spot(1)
         all_images_bright_particles{t}(find_spot(2),3) = 1;
     else 
+        if find_spot(1)
+            one_tracker.position_xy(end,:) = [];% delete multi-assigned pisitions
+        end
         find_spot = one_tracker.FindNextDim(next_dim_particles);
         if find_spot(1)
             all_images_dim_particles{t}(find_spot(2),3) = 1;
