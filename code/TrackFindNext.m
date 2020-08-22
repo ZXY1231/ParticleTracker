ea07e260b7aad20b9f56f8b3ee4ce2d596ef9238
ZXY_win <zhouxy1231@gmail.com>
2020-08-21 17:15:13 -0700

    find next place

--- code/TrackFindNext.m
+++ code/TrackFindNext.m
@@ -9,8 +9,7 @@ function find_spot = TrackFindNext(one_tracker, next_bright_particles, next_dim_
 %         t                       : t indicateds t-th frame of detected spots
 %Output:  find_spot                    :[1, next spot ID] or 0
 
-%provide multi-particles assignment temporary solution, bug: tracker adds
-%posirions before if find_spot()
+%provide multi-particles assignment temporary solution
     
     global all_images_bright_particles all_images_dim_particles
     find_spot = one_tracker.FindNext(next_bright_particles);
