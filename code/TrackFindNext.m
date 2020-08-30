28dadd64f45d3b5538910a276b96727929a14963
ZXY_win <zhouxy1231@gmail.com>
2020-08-30 01:32:15 -0700

    tracker finds next position, links detected spots

--- code/TrackFindNext.m
+++ code/TrackFindNext.m
@@ -16,14 +16,16 @@ function find_spot = TrackFindNext(one_tracker, next_bright_particles, next_dim_
     
     if find_spot(1) && all_images_bright_particles{t}(find_spot(2),3) == 0% multi-particles assignment temporary solution
 %     if find_spot(1)
-        all_images_bright_particles{t}(find_spot(2),3) = 1;
+        all_images_bright_particles{t}(find_spot(2),3) = one_tracker.track_id;
     else 
         if find_spot(1)
             one_tracker.position_xy(end,:) = [];% delete multi-assigned pisitions
+            
+            
         end
         find_spot = one_tracker.FindNextDim(next_dim_particles);
         if find_spot(1)
-            all_images_dim_particles{t}(find_spot(2),3) = 1;
+            all_images_dim_particles{t}(find_spot(2),3) = one_tracker.track_id;
         end
     end
 end
